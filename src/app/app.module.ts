90212cfeac20165e1725a694107f97e50c4e9c7f
Eugene Stativka <stativkaea@gmail.com>
2019-10-01 12:56:36 +0200

    Add PWA capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ScannerModule } from './scanner/scanner.module'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ScannerModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ScannerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
